5c2aa6857ea6c912939fd13e6b6b772d7c9bade9
erik <erikfirdaus03@gmail.com>
2024-01-24 14:21:35 +0700

    mempercantik tampilan produk

--- produk-form-tambah.php
+++ produk-form-tambah.php
@@ -1,15 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Tambah Produk</title>
+    <link rel="stylesheet" href="pelanggan.css">
+    <title>tambah produk</title>
 </head>
+
 <body>
-    <div class="text-center">
-    <h1>isi form produk</h1>
-    <a href="produk-daftar.php" class="btn btn-primary">Balik ke produk</a>
+    <div class="container text-center">
+        <h1>tambah produk</h1>
+    </div>
+    <div class="pelanggan">
+        <div class="formisi">
+            <form action="produk-tambah.php">
+                <div>
+                    <label class="form-label">Nama Produk</label>
+                    <input name="namaproduk" type="text" class="form-control" placeholder="Nama">
+                </div>
+                <div>
+                    <label class="form-label">Harga</label>
+                    <textarea name="harga" class="form-control" rows="2" placeholder="harga"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Stock</label>
+                    <input name="stock" type="text" class="form-control" placeholder="stock">
+                </div>
+                <div class="container text-center">
+                    <a class="btn btn-primary" href="produk-daftar.php">balik ke daftar</a>
+                    <button type="submit" class="btn btn-primary">simpan</button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
+
 </html>
